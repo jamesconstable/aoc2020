7e0a4532daa8afb3f2bc97fe72349079d14bb513
James Constable <jwdconstable@gmail.com>
2020-12-04 13:53:36 +1100

    Refactor Days 1-3 to make solve functions pure

--- 1/main.hs
+++ 1/main.hs
@@ -2,7 +2,7 @@ import Data.List (tails)
 import System.Environment (getArgs)
 
 {-
-   Solver for Day 1 of the Advent of Code 2020.
+   Solver for Day 1 of the Advent of Code 2020
    Problem description here: https://adventofcode.com/2020/day/1
 
    Takes a command line argument indicating which part to solve (1 or 2), reads
@@ -11,16 +11,17 @@ import System.Environment (getArgs)
 
 main :: IO ()
 main = do
+  let run = (>>= putStrLn) . (<$> getContents) . (show .)
   part <- getArgs
   case part of
-    ["1"]     -> solve 2
-    ["2"]     -> solve 3
+    ["1"]     -> run $ solve 2
+    ["2"]     -> run $ solve 3
     otherwise -> putStrLn "Valid options are 1 or 2"
 
-solve :: Int -> IO ()
-solve n = do
-  report <- map read . lines <$> getContents
-  putStrLn $ show $ product $ findNums n report
+solve :: Int -> [Char] -> Int
+solve n input =
+  let report = map read $ lines input
+  in product $ findNums n report
 
 findNums :: Int -> [Int] -> [Int]
 findNums n = head . filter ((== 2020) . sum) . combinations n
--- 3/main.hs
+++ 3/main.hs
@@ -11,16 +11,17 @@ import System.Environment (getArgs)
 
 main :: IO ()
 main = do
+  let run = (>>= putStrLn) . (<$> getContents) . (show .)
   part <- getArgs
   case part of
-    ["1"]     -> solve [(3, 1)]
-    ["2"]     -> solve [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    ["1"]     -> run $ solve [(3, 1)]
+    ["2"]     -> run $ solve [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     otherwise -> putStrLn "Valid options are 1 or 2"
 
-solve :: [(Int, Int)] -> IO ()
-solve slopes = do
-  mapData <- prepareMap . lines <$> getContents
-  putStrLn $ show $ product $ map (countCollisions mapData) slopes
+solve :: [(Int, Int)] -> [Char] -> Int
+solve slopes input =
+  let mapData = prepareMap (lines input)
+  in product $ map (countCollisions mapData) slopes
 
 prepareMap :: [[Char]] -> Array (Int, Int) Int
 prepareMap ls = array ((0, 0), (length ls - 1, length (head ls) - 1)) $ do
