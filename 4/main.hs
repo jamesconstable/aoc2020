be71f3d0897350afc2b7c68ad7cafee28ef76281
James Constable <jwdconstable@gmail.com>
2020-12-05 05:15:31 +1100

    Move validators to top level and consolidate solve

--- 4/main.hs
+++ 4/main.hs
@@ -1,4 +1,3 @@
-import Data.Array ((!), Array, array, bounds)
 import Data.Char (isDigit)
 import Data.Map (findWithDefault, fromList, member, size)
 import System.Environment (getArgs)
@@ -6,11 +5,13 @@ import Text.Read (readMaybe)
 
 {-
    Solver for Day 4 of the Advent of Code 2020
-   Problem description here: https://adventofcode.com/2020/day/3
+   Problem description here: https://adventofcode.com/2020/day/4
 
    Takes a command line argument indicating which part to solve (1 or 2), reads
    input from stdin, and prints solution to stdout.
 -}
+
+data Field = Field { key :: String, value :: String }
 
 main :: IO ()
 main = do
@@ -22,42 +23,49 @@ main = do
     otherwise -> putStrLn "Valid options are 1 or 2"
 
 solve :: Bool -> String -> Int
-solve validateFields input =
+solve validateFields =
   let
-    checkRangedInt min max v = length v == 4 && read v >= min && read v <= max
-    checkHeight v =
-      let (digits, unit) = span isDigit v
-      in case (readMaybe digits, unit) of
-          (Just n, "cm") -> 150 <= n && n <= 193
-          (Just n, "in") -> 59 <= n && n <= 76
-          otherwise      -> False
-    checkHair v =
-      if length v == 7 && head v == '#'
-      then all (`elem` "0123456789abcdef") (tail v)
-      else False
-    checkPID v = length v == 9 && all isDigit v
     requiredFields = fromList [
-      ("byr", checkRangedInt 1920 2002),
-      ("iyr", checkRangedInt 2010 2020),
-      ("eyr", checkRangedInt 2020 2030),
-      ("hgt", checkHeight),
-      ("hcl", checkHair),
+      ("byr", validateInt 4 1920 2002),
+      ("iyr", validateInt 4 2010 2020),
+      ("eyr", validateInt 4 2020 2030),
+      ("hgt", validateHeight),
+      ("hcl", validateHair),
       ("ecl", (`elem` ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"])),
-      ("pid", checkPID)]
-    passports = parseInput input
-    isRequired = (`member` requiredFields) . fst
-    isValid (k, v) = findWithDefault (const False) k requiredFields v
-    hasRequiredFields = (== size requiredFields) . length . filter isRequired
-    hasValidFields = (== size requiredFields) . length . filter isValid
+      ("pid", validateInt 9 minBound maxBound)]
+    isValid (Field k v) = findWithDefault (const False) k requiredFields v
+    filterByFields fn = filter ((== size requiredFields) . length . filter fn)
   in length
-    $ (if validateFields then filter hasValidFields else id)
-    $ filter hasRequiredFields passports
+    . (if validateFields then filterByFields isValid else id)
+    . filterByFields ((`member` requiredFields) . key)
+    . parseInput
 
-parseInput :: String -> [[(String, String)]]
+inRange :: Ord a => a -> a -> a -> Bool
+inRange min max v = min <= v && v <= max
+
+validateInt :: Int -> Int -> Int -> String -> Bool
+validateInt size min max v =
+  length v == size && maybe False (inRange min max) (readMaybe v)
+
+validateHeight :: String -> Bool
+validateHeight v =
+  let (digits, unit) = span isDigit v
+  in case (readMaybe digits, unit) of
+    (Just n, "cm") -> inRange 150 193 n
+    (Just n, "in") -> inRange 59 76 n
+    otherwise      -> False
+
+validateHair :: String -> Bool
+validateHair v =
+  if length v == 7 && head v == '#'
+  then all (`elem` "0123456789abcdef") (tail v)
+  else False
+
+parseInput :: String -> [[Field]]
 parseInput input = parse [] (words <$> lines input)
   where
-    toPairs = map ((\(k, ':':v) -> (k, v)) . break (== ':'))
+    toFields = map ((\(k, ':':v) -> Field k v) . break (== ':'))
     parse acc ts = case ts of
       []            -> [acc]
       ([]:rest)     -> acc : parse [] rest
-      (fields:rest) -> parse (toPairs fields ++ acc) rest
+      (fields:rest) -> parse (toFields fields ++ acc) rest
