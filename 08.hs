cf56241f413778c746427d2d66bdb28cdb20edec
James Constable <jwdconstable@gmail.com>
2020-12-09 04:19:02 +1100

    Factor out common behaviour and improve readability

--- 08.hs
+++ 08.hs
@@ -1,6 +1,4 @@
 import Data.Maybe (fromJust)
-import Data.Map ((!), (!?))
-
 import qualified Data.Map as M
 
 import Runner (runner)
@@ -12,46 +10,46 @@ import Runner (runner)
 
 data Op = Nop Int | Acc Int | Jmp Int | End
 
+data LoopBehaviour = Error | Return deriving Eq
+
+type Program = M.Map Int Op
+
 main :: IO ()
 main = runner solve1 solve2
 
 solve1 :: String -> Int
-solve1 input =
-  let program = M.fromList $ zip [0..] $ zip (parseInput input) (repeat False)
-  in fromJust $ run program True 0 0
+solve1 = fromJust . run Return . parseProgram
 
 solve2 :: String -> Int
-solve2 input =
-  let
-    program = M.fromList $ zip [0..] $ zip (parseInput input) (repeat False)
-    possibleFixes = generatePossibleFixes program
-    outcomes = map (\p -> run p False 0 0) possibleFixes
-  in fromJust $ head $ filter (/= Nothing) outcomes
+solve2 = fromJust . head . filter (/= Nothing) . map (run Error) .
+  generatePossibleFixes . parseProgram
 
-run :: M.Map Int (Op, Bool) -> Bool -> Int -> Int -> Maybe Int
-run program exitOnLoop line acc =
-  let
-    continue (op, _) = run (M.insert line (op, True) program) exitOnLoop
-    onLoop = if exitOnLoop then Just acc else Nothing
-  in case program !? line of
-    Nothing           -> Nothing    -- Invalid line number
-    Just (_, True)    -> onLoop     -- Entering loop
-    Just (End, _)     -> Just acc   -- Successful exit
-    Just s@(Nop x, _) -> continue s (line + 1) acc
-    Just s@(Acc x, _) -> continue s (line + 1) (acc + x)
-    Just s@(Jmp x, _) -> continue s (line + x) acc
+run :: LoopBehaviour -> Program -> Maybe Int
+run loop program = run' (M.fromList $ zip (M.keys program) (repeat False)) 0 0
+  where
+    run' visited line acc =
+      let
+        continue = run' (M.insert line True visited)
+        inLoop = M.findWithDefault False line visited
+      in case (inLoop, M.lookup line program) of
+        (True, _)         -> if loop == Return then Just acc else Nothing
+        (_, Nothing)      -> Nothing       -- Invalid line number
+        (_, Just End)     -> Just acc      -- Successful exit
+        (_, Just (Nop _)) -> continue (line + 1) acc
+        (_, Just (Acc x)) -> continue (line + 1) (acc + x)
+        (_, Just (Jmp x)) -> continue (line + x) acc
 
-generatePossibleFixes :: M.Map Int (Op, a) -> [M.Map Int (Op, a)]
+generatePossibleFixes :: Program -> [Program]
 generatePossibleFixes program = generate' $ M.assocs program
   where
     generate' p = case p of
-      []                 -> []
-      (n, (Nop x, s)):ls -> M.insert n (Jmp x, s) program : generate' ls
-      (n, (Jmp x, s)):ls -> M.insert n (Nop x, s) program : generate' ls
-      _:ls               -> generate' ls
+      []            -> []
+      (n, Nop x):ls -> M.insert n (Jmp x) program : generate' ls
+      (n, Jmp x):ls -> M.insert n (Nop x) program : generate' ls
+      _:ls          -> generate' ls
 
-parseInput :: String -> [Op]
-parseInput = (++ [End]) . map parseLine . lines
+parseProgram :: String -> Program
+parseProgram = M.fromList . zip [0..] . (++ [End]) . map parseLine . lines
   where
     parseInt (s:n) = (if s == '+' then 1 else -1) * read n
     parseLine l = case words l of
