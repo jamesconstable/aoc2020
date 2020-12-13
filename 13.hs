bf96bc4eca01cce87a408ab0dc2ee3a0221cd35c
James Constable <jwdconstable@gmail.com>
2020-12-13 23:07:13 +1100

    Change all Ints to Integer and tidy up

--- 13.hs
+++ 13.hs
@@ -1,5 +1,5 @@
-import Data.List (sort, tails)
-import Data.Maybe (catMaybes, fromJust, isJust, mapMaybe)
+import Data.List (foldl')
+import Data.Maybe (catMaybes, mapMaybe)
 
 import Runner (runner)
 
@@ -11,29 +11,26 @@ import Runner (runner)
 main :: IO ()
 main = runner solve1 solve2
 
-solve1 :: String -> Int
+solve1 :: String -> Integer
 solve1 input =
   let
     (time, buses) = parseInput input
-    (wait, bus) = minimum $ map (\x -> (minutesWait time x, x)) $ catMaybes buses
+    (wait, bus) = minimum $ map (\b -> (waitTime time b, b)) $ catMaybes buses
   in wait * bus
 
 solve2 :: String -> Integer
 solve2 input =
   let
     (_, buses) = parseInput input
-    buses' = map (fmap toInteger) buses
-    getRemainder (i, bus) = bus >>= \b -> Just (b-i, b)
-    busOffsets = mapMaybe getRemainder (zip [0..] buses')
+    getOffset (i, bus) = bus >>= \b -> Just (b-i, b)
+    busOffsets = mapMaybe getOffset $ zip [0..] buses
   in fst $ chineseRemainder busOffsets
 
-minutesWait :: Integral a => a -> a -> a
-minutesWait time bus =
-  let lastBus = time `div` bus
-  in (lastBus + 1) * bus - time
+waitTime :: Integral a => a -> a -> a
+waitTime time bus = bus - time `mod` bus
 
 chineseRemainder :: Integral a => [(a, a)] -> (a, a)
-chineseRemainder = foldr chineseRemainder' (0, 1)
+chineseRemainder = foldl' chineseRemainder' (0, 1)
   where
     chineseRemainder' (r1, m1) (r2, m2) =
       let
@@ -43,23 +40,24 @@ chineseRemainder = foldr chineseRemainder' (0, 1)
 
 modularInverse :: Integral a => a -> a -> a
 modularInverse x m =
-  let (_, i, _) = extendedEuclidean x m
+  let (_, i, _) = gcdExtended x m
   in i `mod` m
 
-extendedEuclidean :: Integral a => a -> a -> (a, a, a)
-extendedEuclidean 0 b = (b, 0, 1)
-extendedEuclidean a b =
-  let (gcd, x, y) = extendedEuclidean (b `mod` a) a
+gcdExtended :: Integral a => a -> a -> (a, a, a)
+gcdExtended 0 b = (b, 0, 1)
+gcdExtended a b =
+  let (gcd, x, y) = gcdExtended (b `mod` a) a
   in (gcd, y - (b `div` a) * x, x)
 
-parseInput :: String -> (Int, [Maybe Int])
+parseInput :: String -> (Integer, [Maybe Integer])
 parseInput input =
   let [l1, l2] = lines input
   in (read l1, parseBuses l2)
 
-parseBuses :: String -> [Maybe Int]
-parseBuses [] = []
-parseBuses (',':t) = parseBuses t
-parseBuses ('x':t) = Nothing : parseBuses t
-parseBuses xs =
-  Just (read (takeWhile (/= ',') xs)) : parseBuses (dropWhile (/= ',') xs)
+parseBuses :: String -> [Maybe Integer]
+parseBuses cs = case cs of
+  []      -> []
+  (',':t) -> parseBuses t
+  ('x':t) -> Nothing : parseBuses t
+  _       -> let (n, t) = break (== ',') cs
+             in Just (read n) : parseBuses t
