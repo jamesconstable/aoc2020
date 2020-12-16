81827de3b294229b7fa328cc5dda6166f0696815
James Constable <jwdconstable@gmail.com>
2020-12-17 05:18:39 +1100

    Finish Day 16 and tidy up

--- 16.hs
+++ 16.hs
@@ -1,8 +1,4 @@
-import Data.Char (digitToInt, intToDigit, isDigit)
-import Data.IntMap (IntMap, elems, empty, insert)
 import Data.List (isPrefixOf, foldl')
-import Data.Maybe (fromJust, listToMaybe)
-import Numeric (readInt, showIntAtBase)
 
 import Runner (runner)
 
@@ -11,93 +7,79 @@ import Runner (runner)
    Problem description: https://adventofcode.com/2020/day/16
 -}
 
-data Rule = Rule {
-  label :: String,
-  r1    :: (Int, Int),
-  r2    :: (Int, Int)
-} deriving (Eq, Show)
+data Rule = Rule { label :: String, ranges :: [(Int, Int)] }
+  deriving (Eq, Show)
+
+type Ticket = [Int]
 
 main :: IO ()
 main = runner solve1 solve2
 
 solve1 :: String -> Int
 solve1 input =
-  let (rules, tickets, _) = parseInput input
+  let (rules, _, tickets) = parseInput input
   in sum $ concatMap (invalidFields rules) tickets
 
 solve2 :: String -> Int
 solve2 input =
   let
-    (rules, tickets, ticket) = parseInput input
-    validTickets =
-      map snd
-      $ filter (null . fst)
-      $ map (\t -> (invalidFields rules t, t)) tickets
-    orderedRules = simplifyRules $ rulesReduce (map (const rules) rules) validTickets
-    myTicket = zip orderedRules ticket
-  in product
-    $ map snd
-    $ filter ((\(Rule l _ _) -> "departure" `isPrefixOf` l) . fst) myTicket
+    (rules, ticket, tickets) = parseInput input
+    validTickets = filter (null . invalidFields rules) tickets
+    allRules = [rules | _ <- rules]
+    orderedRules = eliminate $ filterCandidateRules allRules validTickets
+    labelledTicket = zip (label <$> orderedRules) ticket
+  in product $ snd <$> filter (isPrefixOf "departure" . fst) labelledTicket
 
-invalidFields :: [Rule] -> [Int] -> [Int]
+invalidFields :: [Rule] -> Ticket -> [Int]
 invalidFields rules = filter (not . validByAnyRule rules)
 
+isValid :: Rule -> Int -> Bool
+isValid (Rule _ ranges) v = any (\(start, end) -> start <= v && v <= end) ranges
+
 validByAnyRule :: [Rule] -> Int -> Bool
-validByAnyRule rules v =
-  any (\(Rule _ r1 r2) -> inRange r1 v || inRange r2 v) rules
+validByAnyRule rules v = any (`isValid` v) rules
 
-inRange :: (Int, Int) -> Int -> Bool
-inRange (s, e) v = s <= v && v <= e
+filterCandidateRules :: [[Rule]] -> [Ticket] -> [[Rule]]
+filterCandidateRules = foldl' $ zipWith (\rs f -> filter (`isValid` f) rs)
 
-simplifyRules :: [[Rule]] -> [Rule]
-simplifyRules = map head . fixpoint (\rs -> simplify' (rs, []))
+eliminate :: Eq a => [[a]] -> [a]
+eliminate = map head . fixpoint (eliminate' . zipperAt 0)
+  where
+    remove e = map (filter (/= e))
+    eliminate' (t@(~(x:xs)), bs)
+      | null t    = reverse bs
+      | [x'] <- x = eliminate' (remove x' xs, x : remove x' bs)
+      | otherwise = eliminate' (xs, x:bs)
 
 fixpoint :: Eq a => (a -> a) -> a -> a
 fixpoint f i =
   let i' = f i
   in if i' == i then i' else fixpoint f i'
 
-simplify' :: ([[Rule]], [[Rule]]) -> [[Rule]]
-simplify' ([], bs) = reverse bs
-simplify' (x:xs, bs) =
-  if length x == 1
-  then
-    let
-      xs' = map (filter (\r -> label r /= label (head x))) xs
-      bs' = map (filter (\r -> label r /= label (head x))) bs
-    in simplify' (xs', x:bs')
-  else simplify' (xs, x:bs)
+zipperAt :: Int -> [a] -> ([a], [a])
+zipperAt i xs = (drop i xs, reverse (take i xs))
 
-rulesReduce :: [[Rule]] -> [[Int]] -> [[Rule]]
-rulesReduce = foldl' ruleReduce
-
-ruleReduce :: [[Rule]] -> [Int] -> [[Rule]]
-ruleReduce = zipWith (\rs f -> filter (`valueValidate` f) rs)
-
-valueValidate :: Rule -> Int -> Bool
-valueValidate (Rule _ r1 r2) v = inRange r1 v || inRange r2 v
-
-parseInput :: String -> ([Rule], [[Int]], [Int])
+parseInput :: String -> ([Rule], Ticket, [Ticket])
 parseInput input =
   let
-    (rules, t:_:_:ts) = parseRules $ lines input
-    ticket = parseTicket t
-    tickets = map parseTicket ts
-  in (rules, tickets, ticket)
-
-parseRules :: [String] -> ([Rule], [String])
-parseRules ls =
-  let (ruleLines, "":_:rest) = break (== "") ls
-  in (map parseRule ruleLines, rest)
+    [ruleLines, [_, ticketLine], _:ticketLines] = splitOn [""] $ lines input
+    rules = map parseRule ruleLines
+    ticket = map read $ splitOn "," ticketLine
+    tickets = map (map read . splitOn ",") ticketLines
+  in (rules, ticket, tickets)
 
 parseRule :: String -> Rule
 parseRule l =
   let
-    (label, ':':' ':t) = break (== ':') l
-    (rawRange00, '-':t') = span isDigit t
-    (rawRange01, ' ':'o':'r':' ':t'') = span isDigit t'
-    (rawRange10, '-':rawRange11) = span isDigit t''
-  in Rule label (read rawRange00, read rawRange01) (read rawRange10, read rawRange11)
+    [label, t] = splitOn ": " l
+    rangeSpans = splitOn " or " t
+    ranges = map ((\[s, e] -> (s, e)) . map read . splitOn "-") rangeSpans
+  in Rule label ranges
 
-parseTicket :: String -> [Int]
-parseTicket l = map read $ words $ map (\c -> if c == ',' then ' ' else c) l
+splitOn :: Eq a => [a] -> [a] -> [[a]]
+splitOn s = splitOn' . zipperAt 0
+  where
+    splitOn' (t@(~(x:xs)), bs)
+      | null t           = [reverse bs]
+      | s `isPrefixOf` t = reverse bs : splitOn' (drop (length s) t, [])
+      | otherwise        = splitOn' (xs, x:bs)
