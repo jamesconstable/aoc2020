83044952dfd363c0ea1137d9751e70651f7e7dde
James Constable <jwdconstable@gmail.com>
2020-12-19 20:20:03 +1100

    Add solver for Day 19

---
+++ 19.hs
@@ -0,0 +1,59 @@
+import Data.IntMap (IntMap, (!), insert, fromList)
+import Data.List (foldl', isPrefixOf)
+
+import Runner (runner)
+
+{-|
+   Solver for Day 19 of the Advent of Code 2020
+   Problem description: https://adventofcode.com/2020/day/19
+-}
+
+type Rule = Either Char [[Int]]
+
+main :: IO ()
+main = runner solve1 solve2
+
+solve1 :: String -> Int
+solve1 input =
+  let (rules, messages) = parseInput input
+  in length $ filter (matches 0 rules) messages
+
+solve2 :: String -> Int
+solve2 input =
+  let
+    (rules, messages) = parseInput input
+    rules' =
+      insert 11 (Right [[42, 31], [42, 11, 31]])
+      $ insert 8 (Right [[42], [42, 8]]) rules
+  in length $ filter (matches 0 rules') messages
+
+matches :: Int -> IntMap Rule -> String -> Bool
+matches ruleNumber rules = any null . dropMatches ruleNumber
+  where
+    dropMatches ruleNumber s = case rules ! ruleNumber of
+      Left c   -> if [c] `isPrefixOf` s then [tail s] else []
+      Right os -> concatMap (foldl' (flip (concatMap . dropMatches)) [s]) os
+
+parseInput :: String -> (IntMap Rule, [String])
+parseInput input =
+  let [rules, messages] = splitOn [""] $ lines input
+  in (fromList $ map parseRule rules, messages)
+
+parseRule :: String -> (Int, Rule)
+parseRule s =
+  let
+    [n, ruleBody] = splitOn ": " s
+    ruleOptions = splitOn " | " ruleBody
+    ruleValue = if head ruleOptions !! 0 == '"'
+      then Left (head ruleOptions !! 1)
+      else Right $ map (map read . splitOn " ") ruleOptions
+  in (read n, ruleValue)
+
+splitOn :: Eq a => [a] -> [a] -> [[a]]
+splitOn s = splitOn' . zipperAt 0
+  where
+    splitOn' (t@(~(x:xs)), bs)
+      | null t           = [reverse bs]
+      | s `isPrefixOf` t = reverse bs : splitOn' (drop (length s) t, [])
+      | otherwise        = splitOn' (xs, x:bs)
+    zipperAt i xs = (drop i xs, reverse (take i xs))
